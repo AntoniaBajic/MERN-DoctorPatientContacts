59bef8724319eb173803431c97ab8bbe1409bbb2
Antonia Bajic <antoniaa.bajic@gmail.com>
2020-06-09 21:20:09 +0200

    Frontend navbar created

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Doctor Patient Contacts</h1>
+      <p className='my-1'>
+        This is a full stack React app for adding and updating doctor and
+        patients contacts
+      </p>
+    </div>
+  );
+};
+
+export default About;
