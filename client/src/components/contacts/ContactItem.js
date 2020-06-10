994e086a1daf9421c34ca6256a62365c9127a653
Antonia Bajic <antoniaa.bajic@gmail.com>
2020-06-10 20:40:16 +0200

    Delete contact  added

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,31 +1,45 @@
-import React from "react";
-import PropTypes from 'prop-types'
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import ContactContext from '../../context/contact/contactContext';
 
 const ContactItem = ({ contact }) => {
+  const contactContext = useContext(ContactContext);
+  const { deleteContact } = contactContext;
   const { id, name, email, phone, type } = contact;
+
+  const onDelete = () => {
+    deleteContact(id);
+  };
   return (
     <div className='card bg-light'>
       <h3 className='text-primary text-left'>
-        {name}{" "}
-        <span style={{ float: 'right' }}
+        {name}{' '}
+        <span
+          style={{ float: 'right' }}
           className={
-            'badge' + (type === 'doctor' ? "badge-primary" : "badge-patient")
+            'badge' + (type === 'doctor' ? 'badge-primary' : 'badge-patient')
           }
         >
           {type.charAt(0).toUpperCase() + type.slice(1)}
         </span>
       </h3>
       <ul className='list'>
-        {email && (<li>
-          <i className="fas fa-envelope-open text-patient"></i> {email}
-        </li>)}
-        {phone && (<li>
-          <i className="fas fa-phone text-patient"></i> {phone}
-        </li>)}
+        {email && (
+          <li>
+            <i className='fas fa-envelope-open text-patient'></i> {email}
+          </li>
+        )}
+        {phone && (
+          <li>
+            <i className='fas fa-phone text-patient'></i> {phone}
+          </li>
+        )}
       </ul>
       <p>
         <button className='btn btn-patient btn-sm'>Edit</button>
-        <button className='btn btn-danger btn-sm'>Delete</button>
+        <button className='btn btn-danger btn-sm' onClick={onDelete}>
+          Delete
+        </button>
       </p>
     </div>
   );
@@ -33,5 +47,5 @@ const ContactItem = ({ contact }) => {
 
 ContactItem.propTypes = {
   contact: PropTypes.object.isRequired,
-}
+};
 export default ContactItem;
