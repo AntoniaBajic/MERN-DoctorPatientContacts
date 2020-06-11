d7c0d857e8ed2695700a1b927a431804874b63a8
Antonia Bajic <antoniaa.bajic@gmail.com>
2020-06-12 01:32:22 +0200

    Search contacts added

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Contacts from '../contacts/Contacts';
 import ContactForm from '../contacts/ContactForm';
+import ContactFilter from '../contacts/ContactsFilter';
+import ContactsFilter from '../contacts/ContactsFilter';
 
 const Home = () => {
   return (
@@ -9,6 +11,7 @@ const Home = () => {
         <ContactForm />
       </div>
       <div>
+        <ContactsFilter />
         <Contacts />
       </div>
     </div>
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -38,6 +38,7 @@ const ContactState = (props) => {
       },
     ],
     current: null,
+    filtered: null,
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -64,19 +65,26 @@ const ContactState = (props) => {
     dispatch({ type: UPDATE_CONTACT, payload: contact });
   };
   // Filter Contacts
-
+  const filterContact = (text) => {
+    dispatch({ type: FILTER_CONTACTS, payload: text });
+  };
   // Clear Filter
-
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER });
+  };
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
         current: state.current,
+        filtered: state.filtered,
         addContact,
         deleteContact,
         setCurrent,
         clearCurrent,
         updateContact,
+        filterContact,
+        clearFilter,
       }}
     >
       {props.children}
